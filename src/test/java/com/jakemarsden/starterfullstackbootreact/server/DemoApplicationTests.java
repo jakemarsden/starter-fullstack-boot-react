a69cdc6f28ff54b79626a0914fc4df619e4e2fc7
Jake Marsden <jakemarsdenjm@gmail.com>
2020-04-15 11:03:17 +1200

    Initial server boilerplate from Spring Initializr
    Command used:
    
    curl https://start.spring.io/starter.tgz \
      -d type=gradle-project \
      -d language=java \
      -d javaVersion=11 \
      -d bootVersion=2.2.6.RELEASE \
      -d groupId=com.jakemarsden.starter-fullstack-boot-react \
      -d artifactId=server \
      -d version=0.0.1-SNAPSHOT \
      -d dependencies=web,configuration-processor,devtools \
      | tar -xzvf -

---
+++ src/test/java/com/jakemarsden/starterfullstackbootreact/server/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakemarsden.starterfullstackbootreact.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
