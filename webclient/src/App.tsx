9451855e91bb3f58cfca7477b2244e0e4c6aea92
Jake Marsden <jakemarsdenjm@gmail.com>
2020-04-15 14:49:00 +1200

    Tweak webclient

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header id="header">
+        <h1>starter-fullstack-boot-react</h1>
       </header>
-    </div>
+      <section id="content">
+        <p>Placeholder content</p>
+      </section>
+    </React.Fragment>
   );
 }
 
