c5cbd475b9df82d26ac50aed2009c0e60d96b4a7
Jake Marsden <jakemarsdenjm@gmail.com>
2020-04-15 12:23:12 +1200

    Move server to subproject and other tweaks

---
+++ server/src/test/java/com/jakemarsden/starterfullstackbootreact/server/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.jakemarsden.starterfullstackbootreact.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+  @Test
+  @SuppressWarnings("JUnitTestMethodWithNoAssertions")
+  void contextLoads() {
+    // ensure the application context can initialize only, no need to assert anything
+  }
+}
